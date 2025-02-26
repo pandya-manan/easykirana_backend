1ae35c238165891e03934e0222a92a9b7f76b9ef
manan.pandya <manan.pandya@globallogic.com>
2025-02-26 18:25:39 +0530

    Added config code for search by category by exposing category id

--- spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/easykirana/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/easykirana/ecommerce/dao/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.easykirana.ecommerce.dao;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -9,4 +11,5 @@ import com.easykirana.ecommerce.entity.Product;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long>{
 
+	Page<Product> findByCategoryId(@Param("id")Long id,Pageable pageable);
 }
