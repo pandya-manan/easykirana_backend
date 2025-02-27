be8339e674c902e083837d5072f517c8669dcf59
manan.pandya <manan.pandya@globallogic.com>
2025-02-27 18:55:06 +0530

    Added search by keyword and product detail view

--- spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/easykirana/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/easykirana/ecommerce/dao/ProductRepository.java
@@ -3,7 +3,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.easykirana.ecommerce.entity.Product;
@@ -12,4 +11,6 @@ import com.easykirana.ecommerce.entity.Product;
 public interface ProductRepository extends JpaRepository<Product,Long>{
 
 	Page<Product> findByCategoryId(@Param("id")Long id,Pageable pageable);
+	
+	Page<Product> findByNameContaining(@Param("name")String name,Pageable page);
 }
